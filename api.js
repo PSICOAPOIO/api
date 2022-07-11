1356e59a08e36eadca82f5e019cb85214f31e316
PSICOAPOIO <102975161+PSICOAPOIO@users.noreply.github.com>
2022-07-11 07:56:11 -0300

    Add files via upload

---
+++ api.js
@@ -0,0 +1,13 @@
+const express=require('express');
+
+const app = express();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+//const router=express.Router();
+const route = require("./routers/route");
+app.use('/', route);
+
+
+module.exports = app;
